c9c2709f5590805e07270b154447c441d07aeb22
struk <halynkastru@gmail.com>
2022-04-10 23:07:06 +0200

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Actor from "./components/Actor";
+import Users from "./components/Users";
+
+const actors = [
+    {id:1, name: 'Tom', img: "https://w7.pngwing.com/pngs/859/427/png-transparent-tom-cat-jerry-mouse-golden-age-of-american-animation-tom-and-jerry-cartoon-tom-jerry-mammal-cat-like-mammal-heroes.png"},
+    {id:2, name: 'Jerry', img: "https://e7.pngegg.com/pngimages/30/563/png-clipart-jerry-mouse-tom-cat-tom-and-jerry-tom-and-jerry-mammal-cat-like-mammal.png"},
+    {id:3, name: 'Spike', img: "https://upload.wikimedia.org/wikipedia/en/e/e4/Spike_and_Tyke_%28characters%29.png"},
+    {id:4, name: 'Tyke', img: "https://i.pinimg.com/564x/83/6f/da/836fdab05c1eb30da94412f57e1c8020.jpg"}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {/*{actors.map(actor => <Actor key={actor.id} name={actor.name} img={actor.img}/>)}*/}
+    <Users/>
     </div>
   );
 }
