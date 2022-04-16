60dd92d7adfe705eb85d56dfec75122e28e04ca7
struk <halynkastru@gmail.com>
2022-04-16 23:52:54 +0200

    homework 1

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,10 @@
 import {useState, useEffect} from 'react'
+import {usersService} from '../../services/users.service';
 
 export const Users = () => {
 const [users, setUsers] = useState();
 useEffect(() => {
-
+    usersService.getAllUsers().then(value => setUsers(value))
 }, [])
     return (
         <div>
