e18b7110890333b0e2c0842c61a9bfc8016f99ae
struk <halynkastru@gmail.com>
2022-05-08 13:55:33 +0200

    lesson 3

--- src/validators/car.validator.js
+++ src/validators/car.validator.js
@@ -8,7 +8,7 @@ const carValidator = Joi.object({
     model:Joi.string().regex(/^[a-zA-ZА-яїЇіІёЁ]{1,20}$/).required().messages({
         'string.pattern.base':'Тільки букви мін 1 символ максимум 20'
     }),
-    price:Joi.number().min(0).max(1000000).required(), // якщо не вписуємо messages то буде помилка описана по замовчуванні
+    price:Joi.number().min(0).max(1000000).required(), // якщо не вписуємо messages то буде помилка описана по стандарту
     year:Joi.number().min(1990).max(new Date().getFullYear()).required()
 }); // це все додаємо в нашу форму у useForm(сюди)
 
