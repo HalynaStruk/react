7d62679bf31efd83eb45fce222871cd345d011fe
struk <halynkastru@gmail.com>
2022-05-30 16:52:40 +0200

    lesson 4

--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import {userService} from "../../services";
+import {User} from "../../components";
 
 const UsersPage = () => {
+    const [users, setUsers] = useState();
+
+    useEffect(()=>{
+        userService.getAll().then(({data})=>setUsers(data))
+    }, [])
+
     return (
         <div>
-            users
+            {users.map(user => <User key={user.id} user={user}/>)}
         </div>
     );
 };
