c4a6c3872f22567c4e727b41605dcce7ce31e17e
struk <halynkastru@gmail.com>
2022-05-31 13:29:57 +0200

    lesson 4 + homework 4 + lesson 5

--- src/pages/PostPage/PostPage.js
+++ src/pages/PostPage/PostPage.js
@@ -1,27 +1,44 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {Outlet, useParams} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {Outlet, useParams, useSearchParams} from "react-router-dom";
 
 import {PostService} from "../../services";
 import {Post} from "../../components";
-import {MyContext} from "../../index";
+
 
 const PostPage = () => {
     const {userId} = useParams();
     const [posts, setPosts] = useState([]);
-    const value = useContext(MyContext);
+    const [query, setQuery] = useSearchParams({page:'1'})
+
     useEffect(()=>{
         if (userId) {
             PostService.getByUserId(userId).then(({data}) => setPosts(data))
-            console.log(value);
-            value.name = 'Olia';
+
         } else {
-            PostService.getAll().then(({data}) => setPosts(data))
+            PostService.getAll(query.get('page'), 10).then(({data}) => setPosts(data))
         }
-    }, [userId])
+    }, [userId, query])
+
+    const nextPage = () => {
+        // let page = query.get('page');
+        // page = +page + 1
+        // setQuery({page:page.toString()})
+
+        // інший спосіб
+        const queryObj = Object.fromEntries(query.entries()) // зробивши з query.entries() - Object ми можево витягнути
+        // з нього всі його значення, які будуть прописані в урлі
+        // query.entries() повертає ітератор
+        // (грубо кажучи, це як витягувати з масиву по одній штуці)
+        queryObj.page++ // те саме що let page = query.get('page'); page = +page + 1
+        setQuery(queryObj);
+    }
 
     return (
         <div style={{display:'flex'}}>
-            <div>{posts.map(post => <Post key={post.id} post={post} flag={!userId}/>)}</div>
+            <div>
+                {posts.map(post => <Post key={post.id} post={post} flag={!userId}/>)}
+                <button onClick={()=>nextPage()}>Next</button>
+            </div>
             <div><Outlet/></div>
         </div>
     );
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -2,7 +2,12 @@ import {axiosService} from "./axios.service";
 import {urls} from "../constants";
 
 const PostService = {
-    getAll:()=>axiosService.get(urls.posts),
+    getAll:(page, _limit=2)=>axiosService.get(urls.posts,  {
+        params:{
+            _start:(page-1)*_limit, // тут вказали скільки постів пропустити
+            _limit:_limit // оскільки кллюч і значення співпадають то можливий такий запис : _limit
+        }
+    }),
     getById:(id)=>axiosService.get(`${urls.posts}/${id}`),
     getByUserId:(id)=>axiosService.get(`${urls.users}/${id}/posts`)
 }
