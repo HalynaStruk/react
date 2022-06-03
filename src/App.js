0888bc3a2cbbf9090d7aaca0e485309d0b6c0101
struk <halynkastru@gmail.com>
2022-06-03 15:36:00 +0200

    redux base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+
+
 
 const App = () => {
+
+    let state = useSelector(state => state); // тут ми дістаємось
+    // до повертаймого значення reducer, який буде символізувати поточний стан
+    let dispatch = useDispatch() // за допомогою useDispatch() ми запихаємо всі значення в action
+    //  завдяки useDispatch, useSelector ми можемо в будь якій компоненті використовувати
+    //  функції( в даному випадку це reducer())
+
     return (
         <div>
-          
+          <button onClick={()=>{
+              dispatch({type:'INC',payload: 1})
+              // dispatch викликає reducer
+              // {type, payload} це наш обєкт action
+          }}>increment</button>
+
+          <button onClick={()=>{
+              dispatch({type:'DEC',payload: 1})
+          }}>decrement</button>
+
+          <button onClick={()=>{
+              dispatch({type:'RESET',payload: 0})
+          }}>reset</button>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
+
+function reducer(state = 0, action) {
+    // state це початковий стан
+    switch (action.type) {
+        case 'INC':
+            return state + action.payload
+        case 'DEC':
+            return state - action.payload
+        case 'RESET':
+            return state = action.payload
+        default:
+            return state;
+    }
+}
+
+let store = createStore(reducer) // createStore() це функція яка має доступ до
+// загального сховища але змінювати його не може
+
+
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    )
